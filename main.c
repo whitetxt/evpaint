1ec57a8a558cbc9dd46d1273de6274b6ae402298
whitetxt <54776821+whitetxt@users.noreply.github.com>
2024-01-24 14:55:58 +0000

    Fixed huge memory leak

--- main.c
+++ main.c
@@ -6,129 +6,148 @@ bool active = true;
 SDL_Rect *canvasPreviews = NULL;
 size_t canvasPreviewNum = 0;
 
-void error_caller(char *err, int type, const char *file, const int line) {
-	switch (type) {
-		case ERROR_TYPE_SDL:
-			printf("Error [%s:%d]: %s%s\n", file, line, err, SDL_GetError());
-			break;
-		case ERROR_TYPE_IMAGE:
-			printf("Error [%s:%d]: %s%s\n", file, line, err, IMG_GetError());
-			break;
-		case ERROR_TYPE_TTF:
-			printf("Error [%s:%d]: %s%s\n", file, line, err, TTF_GetError());
-			break;
-		case ERROR_TYPE_NORMAL:
-		default:
-			printf("Error [%s:%d]: %s", file, line, err);
-			break;
-	}
+void error_caller(char *err, int type, const char *file, const int line)
+{
+    switch (type)
+    {
+    case ERROR_TYPE_SDL:
+        printf("Error [%s:%d]: %s%s\n", file, line, err, SDL_GetError());
+        break;
+    case ERROR_TYPE_IMAGE:
+        printf("Error [%s:%d]: %s%s\n", file, line, err, IMG_GetError());
+        break;
+    case ERROR_TYPE_TTF:
+        printf("Error [%s:%d]: %s%s\n", file, line, err, TTF_GetError());
+        break;
+    case ERROR_TYPE_NORMAL:
+    default:
+        printf("Error [%s:%d]: %s", file, line, err);
+        break;
+    }
 }
 
-void warn_caller(char *err, const char *file, const int line) {
-	printf("Warning [%s:%d]: %s", file, line, err);
+void warn_caller(char *err, const char *file, const int line)
+{
+    printf("Warning [%s:%d]: %s", file, line, err);
 }
 
-double render() {
-	// Returns rendertime
-	clock_t t;
-	t = clock();
-	canvasPreviews = realloc(canvasPreviews, canvasPreviewNum * sizeof(*canvasPreviews));
-	SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
-	SDL_RenderClear(renderer);
-	SDL_Rect tmp = {0, 0, width, 70};
-	SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
-	SDL_RenderFillRect(renderer, &tmp);
-	SDL_Rect tmp2 = {0, 70, width, height > 750 ? height / 10 : 75};
-	UIOffsetY = height > 750 ? height / 10 : 75;
-	SDL_SetRenderDrawColor(renderer, 125, 125, 125, 255);
-	SDL_RenderFillRect(renderer, &tmp2);
-	int off = 0;
-	for (size_t i = 0; i < numCanvases; i++) {
-		SDL_UpdateTexture(canvases[i].tex, NULL, canvases[i].pixels, canvases[i].width * sizeof(uint32_t));
-		canvasPreviews[i].x = 10 + off;
-		canvasPreviews[i].y = 10;
-		canvasPreviews[i].w = 50;
-		canvasPreviews[i].h = 50;
-		off += 60;
-		SDL_RenderCopy(renderer, canvases[i].tex, NULL, &canvasPreviews[i]);
-	}
-	renderCanvas(&canvases[currentCanvas]);
-	int offset = 0;
-	UIOffsetY += 70;
-	UIOffsetY *= 1.4;
-	for (size_t i = 0; i < NumButtons; i++) {
-		renderButton(&Buttons[i], &offset);
-	}
-	for (size_t i = 0; i < NumSliders; i++) {
-		renderSlider(&Sliders[i], &offset);
-	}
-	for (size_t i = 0; i < NumSwitches; i++) {
-		renderSwitch(Switches[i], i);
-	}
-	SDL_SetRenderDrawColor(renderer, (int)Sliders[RIndex].value, (int)Sliders[GIndex].value, (int)Sliders[BIndex].value, 255);
-	SDL_RenderFillRect(renderer, &ColourPreview);
-	SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
-	SDL_RenderPresent(renderer);
-	t = clock() - t;
-	double time = ((double)t) / CLOCKS_PER_SEC;
-	return time;
+double render()
+{
+    // Returns rendertime
+    clock_t t;
+    t = clock();
+    canvasPreviews = realloc(canvasPreviews, canvasPreviewNum * sizeof(*canvasPreviews));
+    SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
+    SDL_RenderClear(renderer);
+    SDL_Rect tmp = {0, 0, width, 70};
+    SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
+    SDL_RenderFillRect(renderer, &tmp);
+    SDL_Rect tmp2 = {0, 70, width, height > 750 ? height / 10 : 75};
+    UIOffsetY = height > 750 ? height / 10 : 75;
+    SDL_SetRenderDrawColor(renderer, 125, 125, 125, 255);
+    SDL_RenderFillRect(renderer, &tmp2);
+    int off = 0;
+    for (size_t i = 0; i < numCanvases; i++)
+    {
+        SDL_UpdateTexture(canvases[i].tex, NULL, canvases[i].pixels, canvases[i].width * sizeof(uint32_t));
+        canvasPreviews[i].x = 10 + off;
+        canvasPreviews[i].y = 10;
+        canvasPreviews[i].w = 50;
+        canvasPreviews[i].h = 50;
+        off += 60;
+        SDL_RenderCopy(renderer, canvases[i].tex, NULL, &canvasPreviews[i]);
+    }
+    renderCanvas(&canvases[currentCanvas]);
+    int offset = 0;
+    UIOffsetY += 70;
+    UIOffsetY *= 1.4;
+    for (size_t i = 0; i < NumButtons; i++)
+    {
+        renderButton(&Buttons[i], &offset);
+    }
+    for (size_t i = 0; i < NumSliders; i++)
+    {
+        renderSlider(&Sliders[i], &offset);
+    }
+    for (size_t i = 0; i < NumSwitches; i++)
+    {
+        renderSwitch(Switches[i], i);
+    }
+    SDL_SetRenderDrawColor(renderer, (int)Sliders[RIndex].value, (int)Sliders[GIndex].value, (int)Sliders[BIndex].value, 255);
+    SDL_RenderFillRect(renderer, &ColourPreview);
+    SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
+    SDL_RenderPresent(renderer);
+    t = clock() - t;
+    double time = ((double)t) / CLOCKS_PER_SEC;
+    return time;
 }
 
-int initAll() {
-	if (initSDL() != 0) {
-		return 1;
-	}
-	if (initTTF() != 0) {
-		return 1;
-	}
-	if (initImage() != 0) {
-		return 1;
-	}
-	return 0;
+int initAll()
+{
+    if (initSDL() != 0)
+    {
+        return 1;
+    }
+    if (initTTF() != 0)
+    {
+        return 1;
+    }
+    if (initImage() != 0)
+    {
+        return 1;
+    }
+    return 0;
 }
 
-void quitAll() {
-	quitTTF();
-	quitImage();
-	quitSDL();
+void quitAll()
+{
+    quitTTF();
+    quitImage();
+    quitSDL();
 }
 
-void quit() {
-	active = false;
+void quit()
+{
+    active = false;
 }
 
-int main(int argc, char **argv) {
-	(void)argc;
-	(void)argv;
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "best");
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "best");
 
-	printf("Started.\n");
-	if (initAll() != 0) {
-		error("Couldn't initialise.\n", ERROR_TYPE_NORMAL);
-		return 1;
-	}
+    printf("Started.\n");
+    if (initAll() != 0)
+    {
+        error("Couldn't initialise.\n", ERROR_TYPE_NORMAL);
+        return 1;
+    }
 
-	Uint32 winFlags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
-	width = 1600;
-	height = 900;
-	UIOffsetY = height > 750 ? height / 10 : 75;
-	window = SDL_CreateWindow("Paint but good", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, winFlags);
+    Uint32 winFlags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
+    width = 1600;
+    height = 900;
+    UIOffsetY = height > 750 ? height / 10 : 75;
+    window = SDL_CreateWindow("Paint but good", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, winFlags);
 
-	if (!window) {
-		error("Failed to create window.\n", ERROR_TYPE_SDL);
-		quitAll();
-		return 1;
-	}
+    if (!window)
+    {
+        error("Failed to create window.\n", ERROR_TYPE_SDL);
+        quitAll();
+        return 1;
+    }
 
-	Uint32 rendFlags = SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED;
-	renderer = SDL_CreateRenderer(window, -1, rendFlags);
-	initFont();
-	createUI();
-	createCanvas(0, 0);
-	while (active) {
-		handleEvents();
-		render();
-	}
-	quitAll();
-	return 0;
+    Uint32 rendFlags = SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED;
+    renderer = SDL_CreateRenderer(window, -1, rendFlags);
+    initFont();
+    createUI();
+    createCanvas(0, 0);
+    while (active)
+    {
+        handleEvents();
+        render();
+    }
+    SDL_DestroyRenderer(renderer);
+    quitAll();
+    return 0;
 }
--- paint.c
+++ paint.c
@@ -4,192 +4,223 @@ Canvas *canvases = NULL;
 size_t numCanvases = 0;
 size_t currentCanvas = 0;
 
-void renderCanvas(Canvas *canvas) {
-	SDL_UpdateTexture(canvas->tex, NULL, canvas->pixels, canvas->width * sizeof(uint32_t));
-	canvas->rect.x = (width - UIOffsetX) / 2 - canvas->rect.w / 2;
-	canvas->rect.y = (height - UIOffsetY) / 2 - canvas->rect.h / 2;
-	SDL_RenderCopy(renderer, canvas->tex, NULL, &canvas->rect);
+void renderCanvas(Canvas *canvas)
+{
+    SDL_UpdateTexture(canvas->tex, NULL, canvas->pixels, canvas->width * sizeof(uint32_t));
+    canvas->rect.x = (width - UIOffsetX) / 2 - canvas->rect.w / 2;
+    canvas->rect.y = (height - UIOffsetY) / 2 - canvas->rect.h / 2;
+    SDL_RenderCopy(renderer, canvas->tex, NULL, &canvas->rect);
 }
 
-int saveCanvas(Canvas canvas, char *filename) {
-	/*
-	Saves the canvas parameter to the file
-	specified.
-	Returns 0 if success, 1 if failure.
-	*/
-	SDL_Texture *target = SDL_GetRenderTarget(renderer);
-	if (SDL_SetRenderTarget(renderer, canvas.tex) != 0) {
-		return 1;
-	}
-	SDL_Surface *surface = SDL_CreateRGBSurface(0, canvas.width, canvas.height, 32, 0, 0, 0, 0);
-	if (!surface) {
-		return 1;
-	}
-	if (SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels, surface->pitch) != 0) {
-		return 1;
-	}
-	char *ext = strrchr(filename, '.');
-	if (!ext) {
-		return 1;
-	}
-	if (strcmp(ext, ".jpg") == 0) {
-		IMG_SaveJPG(surface, filename, 100);
-	} else if (strcmp(ext, ".bmp") == 0) {
-		SDL_SaveBMP(surface, filename);
-	} else if (strcmp(ext, ".png") == 0)  {
-		IMG_SavePNG(surface, filename);
-	}
-	SDL_FreeSurface(surface);
-	if (SDL_SetRenderTarget(renderer, target) != 0) {
-		return 1;
-	}
-	return 0;
+int saveCanvas(Canvas canvas, char *filename)
+{
+    /*
+    Saves the canvas parameter to the file
+    specified.
+    Returns 0 if success, 1 if failure.
+    */
+    SDL_Texture *target = SDL_GetRenderTarget(renderer);
+    if (SDL_SetRenderTarget(renderer, canvas.tex) != 0)
+    {
+        return 1;
+    }
+    SDL_Surface *surface = SDL_CreateRGBSurface(0, canvas.width, canvas.height, 32, 0, 0, 0, 0);
+    if (!surface)
+    {
+        return 1;
+    }
+    if (SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels, surface->pitch) != 0)
+    {
+        return 1;
+    }
+    char *ext = strrchr(filename, '.');
+    if (!ext)
+    {
+        return 1;
+    }
+    if (strcmp(ext, ".jpg") == 0)
+    {
+        IMG_SaveJPG(surface, filename, 100);
+    }
+    else if (strcmp(ext, ".bmp") == 0)
+    {
+        SDL_SaveBMP(surface, filename);
+    }
+    else if (strcmp(ext, ".png") == 0)
+    {
+        IMG_SavePNG(surface, filename);
+    }
+    SDL_FreeSurface(surface);
+    if (SDL_SetRenderTarget(renderer, target) != 0)
+    {
+        return 1;
+    }
+    return 0;
 }
 
-void saveCurrentCanvas() {
-	printf("Saving picture.\n");
-	char const * lFilterPatterns[3] = {"*.jpg", "*.png", "*.bmp"};
-	char *fn = tinyfd_saveFileDialog("Save location", "out.png", 3, lFilterPatterns, NULL);
-	saveCanvas(canvases[currentCanvas], fn);
-	printf("Canvas saved.\n");
+void saveCurrentCanvas()
+{
+    printf("Saving picture.\n");
+    char const *lFilterPatterns[3] = {"*.jpg", "*.png", "*.bmp"};
+    char *fn = tinyfd_saveFileDialog("Save location", "out.png", 3, lFilterPatterns, NULL);
+    saveCanvas(canvases[currentCanvas], fn);
+    printf("Canvas saved.\n");
 }
 
-int createCanvas(int Cwidth, int Cheight) {
-	/*
-	Creates a brand new white canvas
-	and adds it to the canvases array
-	and sets the currentCanvas to it
-	Returns the index.
-	*/
-	if (Cwidth < 1 || Cheight < 1) {
-		Cwidth = 300;
-		Cheight = 300;
-	}
-	canvases = realloc(canvases, ++numCanvases * sizeof(*canvases));
-	int canvasNum = numCanvases - 1;
-	currentCanvas = numCanvases - 1;
-	canvases[currentCanvas].pixels = malloc(Cwidth * Cheight * sizeof(Uint32));
-	memset(canvases[currentCanvas].pixels, 255, Cwidth * Cheight * sizeof(Uint32));
-	canvases[currentCanvas].tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, Cwidth, Cheight);
-	canvases[currentCanvas].width = Cwidth;
-	canvases[currentCanvas].height = Cheight;
-	canvases[currentCanvas].rect.w = Cwidth;
-	canvases[currentCanvas].rect.h = Cheight;
-	canvases[currentCanvas].rect.x = UIOffsetX;
-	canvases[currentCanvas].rect.y = UIOffsetY;
-	canvasPreviewNum += 1;
-	return canvasNum;
+int createCanvas(int Cwidth, int Cheight)
+{
+    /*
+    Creates a brand new white canvas
+    and adds it to the canvases array
+    and sets the currentCanvas to it
+    Returns the index.
+    */
+    if (Cwidth < 1 || Cheight < 1)
+    {
+        Cwidth = 300;
+        Cheight = 300;
+    }
+    canvases = realloc(canvases, ++numCanvases * sizeof(*canvases));
+    int canvasNum = numCanvases - 1;
+    currentCanvas = numCanvases - 1;
+    canvases[currentCanvas].pixels = malloc(Cwidth * Cheight * sizeof(Uint32));
+    memset(canvases[currentCanvas].pixels, 255, Cwidth * Cheight * sizeof(Uint32));
+    canvases[currentCanvas].tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, Cwidth, Cheight);
+    canvases[currentCanvas].width = Cwidth;
+    canvases[currentCanvas].height = Cheight;
+    canvases[currentCanvas].rect.w = Cwidth;
+    canvases[currentCanvas].rect.h = Cheight;
+    canvases[currentCanvas].rect.x = UIOffsetX;
+    canvases[currentCanvas].rect.y = UIOffsetY;
+    canvasPreviewNum += 1;
+    return canvasNum;
 }
 
-void createCanvasPopup() {
-	char *width = tinyfd_inputBox("New canvas width", "Please enter the width for the new canvas", "");
-	if (!width || atoi(width) < 0) {
-		tinyfd_messageBox("Invalid width", "Invalid width entered.", "ok", "error", 1);
-		return;
-	}
-	char *height = tinyfd_inputBox("New canvas width", "Please enter the width for the new canvas", "");
-	if (!height || atoi(height) < 0) {
-		tinyfd_messageBox("Invalid height", "Invalid height entered.", "ok", "error", 1);
-		return;
-	}
-	int w;
-	int h;
-	if (sscanf(width, "%d", &w) != 1) {
-		tinyfd_messageBox("Invalid width", "Invalid width entered.", "ok", "error", 1);
-		return;
-	}
-	if (sscanf(height, "%d", &h) != 1) {
-		tinyfd_messageBox("Invalid height", "Invalid height entered.", "ok", "error", 1);
-		return;
-	}
-	createCanvas(w, h);
+void createCanvasPopup()
+{
+    char *width = tinyfd_inputBox("New canvas width", "Please enter the width for the new canvas", "");
+    if (!width || atoi(width) < 0)
+    {
+        tinyfd_messageBox("Invalid width", "Invalid width entered.", "ok", "error", 1);
+        return;
+    }
+    char *height = tinyfd_inputBox("New canvas width", "Please enter the width for the new canvas", "");
+    if (!height || atoi(height) < 0)
+    {
+        tinyfd_messageBox("Invalid height", "Invalid height entered.", "ok", "error", 1);
+        return;
+    }
+    int w;
+    int h;
+    if (sscanf(width, "%d", &w) != 1)
+    {
+        tinyfd_messageBox("Invalid width", "Invalid width entered.", "ok", "error", 1);
+        return;
+    }
+    if (sscanf(height, "%d", &h) != 1)
+    {
+        tinyfd_messageBox("Invalid height", "Invalid height entered.", "ok", "error", 1);
+        return;
+    }
+    createCanvas(w, h);
 }
 
-void setPixel(int x, int y) {
-	/*
-	Sets the pixel at the target X and Y
-	Sets it on the current canvas.
-	*/
-	x -= canvases[currentCanvas].rect.x;
-	y -= canvases[currentCanvas].rect.y;
-	if (x > canvases[currentCanvas].width ||
-		x < 0 ||
-		y > canvases[currentCanvas].height ||
-		y < 0) {
-		return;
-	}
-	Uint32 pixelValue = getColour();
-	canvases[currentCanvas].pixels[y * canvases[currentCanvas].height + x] = pixelValue;
+void setPixel(int x, int y)
+{
+    /*
+    Sets the pixel at the target X and Y
+    Sets it on the current canvas.
+    */
+    x -= canvases[currentCanvas].rect.x;
+    y -= canvases[currentCanvas].rect.y;
+    if (x > canvases[currentCanvas].width ||
+        x < 0 ||
+        y > canvases[currentCanvas].height ||
+        y < 0)
+    {
+        return;
+    }
+    Uint32 pixelValue = getColour();
+    canvases[currentCanvas].pixels[y * canvases[currentCanvas].height + x] = pixelValue;
 }
 
-void setLine(int x1, int y1, int x2, int y2) {
-	/*
-	Old algorithm method, bad.
-	if (x1 == x2 || (x1 - x2) == 0) {
-		int currentY = y1;
-		if (y1 > y2) {
-			while (currentY > y2) {
-				currentY--;
-				setPixel(x1, currentY);
-			}
-		} else {
-			while (currentY < y2) {
-				currentY++;
-				setPixel(x1, currentY);
-			}
-		}
-		return;
-	}
-	double grad = (y1 - y2)/(x1 - x2);
-	double yint = y1 - grad * x1;
-	int currentX = x1;
-	if (x1 > x2) {
-		while (currentX > x2) {
-			currentX--;
-			setPixel(currentX, roundI(grad * currentX + yint));
-		}
-	} else {
-		while (currentX < x2) {
-			currentX++;
-			setPixel(currentX, roundI(grad * currentX + yint));
-		}
-	}*/
+void setLine(int x1, int y1, int x2, int y2)
+{
+    /*
+    Old algorithm method, bad.
+    if (x1 == x2 || (x1 - x2) == 0) {
+        int currentY = y1;
+        if (y1 > y2) {
+            while (currentY > y2) {
+                currentY--;
+                setPixel(x1, currentY);
+            }
+        } else {
+            while (currentY < y2) {
+                currentY++;
+                setPixel(x1, currentY);
+            }
+        }
+        return;
+    }
+    double grad = (y1 - y2)/(x1 - x2);
+    double yint = y1 - grad * x1;
+    int currentX = x1;
+    if (x1 > x2) {
+        while (currentX > x2) {
+            currentX--;
+            setPixel(currentX, roundI(grad * currentX + yint));
+        }
+    } else {
+        while (currentX < x2) {
+            currentX++;
+            setPixel(currentX, roundI(grad * currentX + yint));
+        }
+    }*/
 
-	// Blitting method, good.
-	SDL_Texture *target = SDL_GetRenderTarget(renderer);
-	if (SDL_SetRenderTarget(renderer, canvases[currentCanvas].tex) != 0) {
-		return;
-	}
-	x1 -= canvases[currentCanvas].rect.x;
-	y1 -= canvases[currentCanvas].rect.y;
-	x2 -= canvases[currentCanvas].rect.x;
-	y2 -= canvases[currentCanvas].rect.y;
-	SDL_SetRenderDrawColor(renderer, (int)Sliders[RIndex].value, (int)Sliders[GIndex].value, (int)Sliders[BIndex].value, 255);
-	SDL_Surface *surface = SDL_CreateRGBSurface(0, canvases[currentCanvas].width, canvases[currentCanvas].height, 32, 0, 0, 0, 0);
-	if (!surface) {
-		return;
-	}
-	thickLineRGBA(renderer, x1, y1, x2, y2, (int)Sliders[ThickIndex].value, (int)Sliders[RIndex].value, (int)Sliders[GIndex].value, (int)Sliders[BIndex].value, 255);
-	if (SDL_RenderReadPixels(renderer, NULL, 0, canvases[currentCanvas].pixels, surface->pitch) != 0) {
-		return;
-	}
-	SDL_UpdateTexture(canvases[currentCanvas].tex, NULL, canvases[currentCanvas].pixels, canvases[currentCanvas].width * sizeof(uint32_t));
-	if (SDL_SetRenderTarget(renderer, target) != 0) {
-		return;
-	}
+    // Blitting method, good.
+    SDL_Texture *target = SDL_GetRenderTarget(renderer);
+    if (SDL_SetRenderTarget(renderer, canvases[currentCanvas].tex) != 0)
+    {
+        return;
+    }
+    x1 -= canvases[currentCanvas].rect.x;
+    y1 -= canvases[currentCanvas].rect.y;
+    x2 -= canvases[currentCanvas].rect.x;
+    y2 -= canvases[currentCanvas].rect.y;
+    SDL_SetRenderDrawColor(renderer, (int)Sliders[RIndex].value, (int)Sliders[GIndex].value, (int)Sliders[BIndex].value, 255);
+    SDL_Surface *surface = SDL_CreateRGBSurface(0, canvases[currentCanvas].width, canvases[currentCanvas].height, 32, 0, 0, 0, 0);
+    if (!surface)
+    {
+        return;
+    }
+    thickLineRGBA(renderer, x1, y1, x2, y2, (int)Sliders[ThickIndex].value, (int)Sliders[RIndex].value, (int)Sliders[GIndex].value, (int)Sliders[BIndex].value, 255);
+    if (SDL_RenderReadPixels(renderer, NULL, 0, canvases[currentCanvas].pixels, surface->pitch) != 0)
+    {
+        SDL_FreeSurface(surface);
+        return;
+    }
+    SDL_FreeSurface(surface);
+    SDL_UpdateTexture(canvases[currentCanvas].tex, NULL, canvases[currentCanvas].pixels, canvases[currentCanvas].width * sizeof(uint32_t));
+    if (SDL_SetRenderTarget(renderer, target) != 0)
+    {
+        return;
+    }
 }
 
-int roundI(double number) {
-	return (number >= 0) ? (int)(number + 0.5) : (int)(number - 0.5);
+int roundI(double number)
+{
+    return (number >= 0) ? (int)(number + 0.5) : (int)(number - 0.5);
 }
 
-Uint32 getColour() {
-	Uint32 pixelValue = 255 << 24;
-	int R = Sliders[RIndex].value;
-	int G = Sliders[GIndex].value;
-	int B = Sliders[BIndex].value;
-	pixelValue = pixelValue | R << 16;
-	pixelValue = pixelValue | G << 8;
-	pixelValue = pixelValue | B;
-	return pixelValue;
+Uint32 getColour()
+{
+    Uint32 pixelValue = 255 << 24;
+    int R = Sliders[RIndex].value;
+    int G = Sliders[GIndex].value;
+    int B = Sliders[BIndex].value;
+    pixelValue = pixelValue | R << 16;
+    pixelValue = pixelValue | G << 8;
+    pixelValue = pixelValue | B;
+    return pixelValue;
 }
